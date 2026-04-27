f7e7a66788749f18c57135bf69e86fece8c48125
SatanVI <Ahmedbangoura852@gmail.com>
2026-04-27 16:04:09 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
   return (
     <footer className="final-footer">
@@ -7,9 +9,9 @@ const Footer = () => {
           BLACK<span>CORE</span> TECH
         </div>
         <div className="footer-links">
-          <a href="/politique-de-confidentialite">Politique de Confidentialité</a>
-          <a href="/mentions-legales">Mentions Légales</a>
-          <a href="/contact">Contact</a>
+          <Link to="/politique-de-confidentialite">Politique de Confidentialité</Link>
+          <Link to="/mentions-legales">Mentions Légales</Link>
+          <Link to="/contact">Contact</Link>
         </div>
       </div>
       <p>BlackCore Tech © {new Date().getFullYear()} Tous droits réservés</p>
